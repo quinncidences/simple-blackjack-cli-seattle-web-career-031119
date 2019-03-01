17f81aa6e46ff13435444c48ebd1e665a0d0d679
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 01:01:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit?(card_total)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid response."
 end
 
 #####################################################
