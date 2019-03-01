472ceb1fa2ff0ca3ecb58a5ddf98acd8cbd8440d
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 00:56:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,8 +34,9 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input = "h"
-    deal_card
+    card_total += deal_card
   elsif user_input = "s"
+    card_total
   end
 end
 
