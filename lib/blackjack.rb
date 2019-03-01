d8be8049f5bfdc3e38a63098d6d45a342b8c52ee
Quinn Cox <qrc.inpa@gmail.com>
2019-03-01 01:08:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,9 +54,11 @@ end
 
 def runner
   welcome
-  initial_round
-  hit?
+  card_total = initial_round
   until card_total > 21
-  end_game
-
+   card_total = hit?(card_total)
+   display_card_total(card_total)
+ end
+ end_game(card_total)
 end
+runner
